a2021fbbad80d6998d9b824ad9e135faa107d399
JMMedina <txwebdev5@gmail.com>
2020-02-17 20:35:22 +0000

    mongoose install and connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
+//DDBB connection
+mongoose.connect('mongodb+srv://admin-jose:<lyxvlonx>@cluster0-9yqrr.mongodb.net/blog?retryWrites=true&w=majority', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(()=>console.log('DDBB connected'))
+.catch((error) => console.log('DDBB connection error: ', error))
 //Routing
 app.get('/', (req,res)=>{
     res.send('Hello World');
