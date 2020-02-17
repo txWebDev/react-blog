b59998fd4199a55ebbbd6c8d332a8a4f70116af9
JMMedina <txwebdev5@gmail.com>
2020-02-17 22:18:42 +0000

    User registration

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
 const app = express();
 //DDBB connection
 mongoose.connect('mongodb+srv://admin-jose:<lyxvlonx>@cluster0-9yqrr.mongodb.net/blog?retryWrites=true&w=majority', 
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     }
 )
 .then(()=>console.log('DDBB connected'))
 .catch((error) => console.log('DDBB connection error: ', error))
+//Middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 //Routing
 app.get('/', (req,res)=>{
     res.send('Hello World');
 });
+app.post('/api/users/register', (req,res)=>{
+    const user = new User(req.body);
+    user.save((error, userData)=>{
+        if(error) return res.json({sucess: false, err})
+    });
+    return res.status(200).json({sucess: true})
+});
 
-const port = 3000;
+const port = 5000;
 app.listen(port, ()=>console.log(`Server listening on port ${port}`));
